8a1b828c38069a4571f2a1f3d7454c174dc24694
DuncanAngel <duncanangel070@gmail.com>
2024-11-13 12:49:46 +0100

    Changed divs to array

--- Dammen/js/script.js
+++ Dammen/js/script.js
@@ -1,1 +1,42 @@
 console.log("main loaded")
+const checkersBoard = document.querySelector(".board");
+const board = [
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1]
+];
+
+for (let i = 0; i < board.length; i++) {
+    const row = board[i]
+    const isRowEven = i % 2;
+    for (let j = 0; j < row.length; j++) {
+        const isEven = j % 2;
+        const classnames = []
+        if (isRowEven) {
+            if (isEven) {
+                classnames.push("item1");
+            } else {
+                classnames.push("item2");
+            }
+        } else {
+            if (isEven) {
+                classnames.push("item2");
+            } else {
+                classnames.push("item1");
+            }
+        }
+        let pawnHTML = '';
+        if (row[j] === 1) {
+            pawnHTML = '<div class="pawn white"></div>';
+        } else if (row[j] === 2) {
+            pawnHTML = '<div class="pawn black"></div>';
+        }
+        checkersBoard.innerHTML += `<div class="item ${classnames.join(" ")}">${pawnHTML}</div>`;
+    }
+}
+
